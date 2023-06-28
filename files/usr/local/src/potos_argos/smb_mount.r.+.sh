0a7d6e1127cfddc06dcfd1208f22fd19abff6e70
Nicolas Iselin <nicolas.github@iselin.ch>
2023-06-28 22:53:58 +0200

    feat: adjust argos mount to underlying cmd

--- files/usr/local/src/potos_argos/smb_mount.r.+.sh
+++ files/usr/local/src/potos_argos/smb_mount.r.+.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/bash
 
-echo "edit me"
-exit 0 
+DISPLAYNAME=changeme
+SMBUSER=yoursmbuser
+SMBHOST=yoursmbhost
+SMBDOMAIN=yoursmbdomain
+HOMEBASE=$DISPLAYNAME
 
-mountconfig=example
-
-MOUNT="mount_smb_sudo -s $mountconfig -q -m"
-UMOUNT="mount_smb_sudo -s $mountconfig -q -u"
-COUNT="mount_smb_sudo -s $mountconfig -q -c"
-VERIFY="mount_smb_sudo -s $mountconfig -q -v"
+MOUNT="mount_smb_sudo -s $SMBHOST -d $SMBDOMAIN -b $HOMEBASE -n $SMBUSER -q -m"
+UMOUNT="mount_smb_sudo -s $SMBHOST -d $SMBDOMAIN -b $HOMEBASE -n $SMBUSER -q -u"
+COUNT="mount_smb_sudo -s $SMBHOST -d $SMBDOMAIN -b $HOMEBASE -n $SMBUSER -q -c"
+VERIFY="mount_smb_sudo -s $SMBHOST -d $SMBDOMAIN -b $HOMEBASE -n $SMBUSER -v"
 
 count=$($COUNT)
-echo -n "${mountconfig}: "
+echo -n "${DISPLAYNAME}: "
 if [[ $count -eq 0 ]]
 then
   echo -n "$count"
 else
-  if $VERIFY > /dev/null
+  if $VERIFY -t 3 -q > /dev/null
   then
     echo -n "$count"
   else
@@ -27,3 +28,5 @@ echo ""
 echo "---"
 echo "mount | terminal=true bash='$MOUNT; sleep 4; exit'"
 echo "umount | terminal=false bash='$UMOUNT'"
+echo "force umount | terminal=true bash='$UMOUNT -y'; sleep 4; exit"
+echo "verify | terminal=true bash='$VERIFY -t 20'"
