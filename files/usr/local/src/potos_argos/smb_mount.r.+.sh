63e44b5a616c316e9ffa1164d5997741287102c2
Nicolas Iselin <nicolas.github@iselin.ch>
2023-06-29 18:59:40 +0200

    feat: add vpn features to smb_mount

--- files/usr/local/src/potos_argos/smb_mount.r.+.sh
+++ files/usr/local/src/potos_argos/smb_mount.r.+.sh
@@ -6,10 +6,39 @@ SMBHOST=yoursmbhost
 SMBDOMAIN=yoursmbdomain
 HOMEBASE=$DISPLAYNAME
 
+#VPNNAME=yourvpnname
+
 MOUNT="mount_smb_sudo -s $SMBHOST -d $SMBDOMAIN -b $HOMEBASE -n $SMBUSER -q -m"
 UMOUNT="mount_smb_sudo -s $SMBHOST -d $SMBDOMAIN -b $HOMEBASE -n $SMBUSER -q -u"
 COUNT="mount_smb_sudo -s $SMBHOST -d $SMBDOMAIN -b $HOMEBASE -n $SMBUSER -q -c"
 VERIFY="mount_smb_sudo -s $SMBHOST -d $SMBDOMAIN -b $HOMEBASE -n $SMBUSER -v"
+
+
+if [[ ! -z "${VPNNAME}" ]]
+then
+  STARTVPN="nmcli c up ${VPNNAME}"
+  STOPVPN="nmcli c down ${VPNNAME}"
+  VPNSTATE=$( LANG=C nmcli c show "${VPNNAME}" | sed -ne "s/VPN.VPN-STATE: *//p" )
+  if [[ -z "${VPNSTATE}" ]]
+  then
+    VPNSTATE="X - down"
+  elif [[ "${VPNSTATE}" == "5 - VPN connected" ]]
+  then
+    NETGOOD=yes
+  else
+    NETGOOD=no
+  fi
+else
+  NETGOOD=yes
+fi
+
+# the menu title is composed of
+#  $DISPLAYNAME
+#  count of mounts from that host
+#  if the count is non zero, it is shown as
+#     3: when $NETGOOD is yes and $VERIFY successul
+#   (3): when $NETGOOD is yes but $VERIFY failed
+# ((3)): when $NETGOOD is no (does not happen without VPN)
 
 count=$($COUNT)
 echo -n "${DISPLAYNAME}: "
@@ -17,11 +46,16 @@ if [[ $count -eq 0 ]]
 then
   echo -n "$count"
 else
-  if $VERIFY -t 3 -q > /dev/null
+  if [[ "${NETGOOD}" == "yes" ]]
   then
-    echo -n "$count"
+    if $VERIFY -t 3 -q > /dev/null
+    then
+      echo -n "$count"
+    else
+      echo -n "($count)"
+    fi
   else
-    echo -n "($count)"
+    echo -n "(($count))"
   fi
 fi
 echo ""
@@ -30,3 +64,11 @@ echo "mount | terminal=true bash='$MOUNT; sleep 4; exit'"
 echo "umount | terminal=false bash='$UMOUNT'"
 echo "force umount | terminal=true bash='$UMOUNT -y'; sleep 4; exit"
 echo "verify | terminal=true bash='$VERIFY -t 20'"
+if [[ ! -z "${VPNNAME}" ]]
+then
+  echo "---"
+  echo "start vpn | terminal=false bash='$STARTVPN'"
+  echo "stop vpn | terminal=false bash='$STOPVPN'"
+  echo "---"
+  echo "VPN state: $VPNSTATE"
+fi
